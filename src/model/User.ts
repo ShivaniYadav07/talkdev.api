d58cfc35306228a79151988a67c1985a97404734
ShivaniYadav07 <sagaryadavarmy813@gmail.com>
2024-07-01 22:48:29 +0530

    first commit

---
+++ src/model/User.ts
@@ -0,0 +1,65 @@
+import mongoose, {Schema, Document, mongo} from "mongoose";
+
+
+export interface Message extends Document{
+    content: string;
+    createdAt: Date
+}
+
+const MessageSchema: Schema<Message> = new Schema ({
+content: {
+    type: String,
+    required: true
+},
+createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now
+}
+})
+
+export interface User extends Document{
+   username: string;
+   email:string;
+   password: string;
+   verifyCodeExpiry: Date;
+   isVerified: boolean;
+   isAcceptingMessage: boolean;
+   messages: Message []
+}
+
+const UserSchema: Schema<User> = new Schema ({
+    username: {
+        type: String,
+        required: [true, "UserName is required"],
+        trim: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        match: [/.+\@.+\..+/, "please use a valid email address"]
+    },
+    password: {
+        type:String,
+        required: true,
+    },
+    verifyCodeExpiry: {
+        type: Date,
+        required: [true, "Verify code Expiry is required"]
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    isAcceptingMessage: {
+        type: Boolean,
+        default: true
+    },
+    messages: [MessageSchema]
+    })
+
+    const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema)
+
+    export default UserModel;
